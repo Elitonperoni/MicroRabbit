786d7fae9688942585b837b913870a4bd40e032f
Eliton Peroni <eliton_peroni@hotmail.com>
2024-03-29 17:33:05 -0300

    dependências

--- MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit.Banking.Api/Program.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Infra.IoC;
 using Microsoft.EntityFrameworkCore;
@@ -10,12 +11,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+DependencyContainer.RegisterServices(builder.Services);
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Banking Microsservices", Version = "v1" });
+});
 
 builder.Services.AddDbContext<BankingDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("BankingDbConnection"));
 });
+
+builder.Services.AddMediatR(typeof(Program));
 
 var app = builder.Build();
 
@@ -23,7 +31,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microsservice V1");
+    });
 }
 
 app.UseHttpsRedirection();
