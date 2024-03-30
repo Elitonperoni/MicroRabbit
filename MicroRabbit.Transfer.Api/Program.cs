5efd654814e55faa1014e0f90c6594d6a4a25590
Eliton Peroni <eliton_peroni@hotmail.com>
2024-03-30 14:46:25 -0300

    microsservices models

--- MicroRabbit.Transfer.Api/Program.cs
+++ MicroRabbit.Transfer.Api/Program.cs
@@ -1,3 +1,8 @@
+using MediatR;
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Transfer.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +10,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+DependencyContainer.RegisterServices(builder.Services);
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Transfer Microsservices", Version = "v1" });
+});
+
+builder.Services.AddDbContext<TransferDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("TransferDbConnection"));
+});
+
+builder.Services.AddMediatR(typeof(Program));
 
 var app = builder.Build();
 
@@ -13,7 +31,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transfer Microsservice V1");
+    });
 }
 
 app.UseHttpsRedirection();
