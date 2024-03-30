bdce1f4b8fedb76a49c2cde31a9800558ec462b9
Eliton Peroni <eliton_peroni@hotmail.com>
2024-03-29 22:09:41 -0300

    ajustes

---
+++ MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MicroRabbit.Banking.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+
+namespace MicroRabbit.Banking.Domain.CommandHandlers
+{
+    public class TransferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+    {
+        private readonly IEventBus _bus;
+        public TransferCommandHandler(IEventBus bus)
+        {
+            _bus = bus; 
+        }
+
+        public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+            //publish event to RabbitMQ
+            return Task.FromResult(true);
+        }
+    }
+}
